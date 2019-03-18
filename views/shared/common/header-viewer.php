c4ae10b01529d26cde86539fd789996cf53657e8
Josh Romphf <joshr@rclmediatedev.lib.rochester.edu>
2019-03-18 12:30:50 -0400

    creating new prod branch so we can fall back on this if updating goes south

--- views/shared/common/header-viewer.php
+++ views/shared/common/header-viewer.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html class="<?php echo get_theme_option('Style Sheet'); ?>" lang="<?php echo get_html_lang(); ?>">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116130278-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	
+	  gtag('config', 'UA-116130278-1');
+	</script>
+	
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=yes" />
     <?php if ($description = option('description')): ?>
